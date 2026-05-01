f27a214f2cfed14e91a0e2f86cb2f7ceedaa7a6f
bhoomi456 <bhoomij0304@gmail.com>
2026-05-01 13:28:16 +0530

    Update multi_args.rb

--- chapter2/multi_args.rb
+++ chapter2/multi_args.rb
@@ -5,3 +5,12 @@ def employee(name,salary,department)
 
 end
 employee("Bhoomi",20000,"IT")
+
+
+def student(name,age,grade)
+    puts "Student: #{name}"
+    puts "Age: #{age}"
+    puts "Grade: #{grade}"
+
+end
+student("Alice",20,"A")
