c5cd7612d59a2b27e9b0a70c9e7643c385923fc7
bhoomi456 <bhoomij0304@gmail.com>
2026-05-01 16:17:56 +0530

    ticket implementation

---
+++ chapter2/query_ticket.rb
@@ -0,0 +1,15 @@
+ticket = Object.new
+
+def ticket.event
+    "music concert"
+end
+def ticket.venue
+    "Bnagalore Stadium"
+end
+def ticket.date
+    "10 may 2025"
+end
+
+puts ticket.event
+puts ticket.venue
+puts ticket.date
---
+++ chapter2/ticket.rb
@@ -0,0 +1,11 @@
+book = Object.new
+def book.title
+    "The great story"
+end
+
+def book.price
+    200
+end
+
+puts book.title
+puts book.price
