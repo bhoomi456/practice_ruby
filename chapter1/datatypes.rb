fb07dc20f70cee5d1c119eace00227c2169e4e3b
bhoomi456 <bhoomij0304@gmail.com>
2026-05-01 11:42:58 +0530

    chapter1

---
+++ chapter1/datatypes.rb
@@ -0,0 +1,12 @@
+company_name = "labsmart"
+employee_count = 10
+product_price = 199.99
+is_hiring = true
+
+puts company_name
+puts employee_count
+puts product_price
+puts is_hiring
+
+name=gets.chomp
+puts name
