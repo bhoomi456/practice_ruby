03fa8ff863be68484a183ae2d9bcdfcd3e83958a
bhoomi456 <bhoomij0304@gmail.com>
2026-05-14 10:53:51 +0530

    add new file

---
+++ practice/book_library.rb
@@ -0,0 +1,72 @@
+# Create a class Book and a class Library.
+# Each book should have:
+# title
+# author
+# The Library class should store multiple books using an array.
+# Methods:
+# add_book(book)
+# list_books
+# find_book(title)
+
+class Book
+  attr_reader :title, :author
+  def initialize(title, author)
+    @title = title
+    @author = author
+  end
+end
+
+class Library
+  def initialize
+    @books = []
+  end
+
+  def add_book(book)
+    @books << book
+    puts "#{book.title} added successfully"
+  end
+
+  def list_books
+    if @books.empty?
+      puts " No books Available"
+    else
+      puts "Library Books :"
+      @books.each do |book|
+        puts "Title: #{book.title}, Author: #{book.author}"
+      end
+    end
+  end
+
+  def find_book(title)
+    found = @books.find {|book| book.title == title}
+
+    if found
+      puts "Book found"
+      puts "Title: #{found.title}"
+      puts "Author: #{found.author}"
+    else
+      puts "Book not found"
+    end
+  end
+end
+
+
+book1 = Book.new("Ruby Basics", "David")
+book2 = Book.new("Rails Guide", "Roy")
+
+library = Library.new
+
+library.add_book(book1)
+library.add_book(book2)
+
+library.list_books
+
+library.find_book("Ruby Basics")
+library.find_book("Ruby")
+
+
+
+  
+    
+
+
--- practice/hospital.rb
+++ practice/hospital.rb
@@ -5,6 +5,9 @@ module HospitalHelper
 end
 
 class Doctor
+  include HospitalHelper
+
+  attr_reader :name
   def initialize(name, specialization, salary, doctor_id, experience)
     @name = name
     @specialization = specialization
@@ -14,7 +17,63 @@ class Doctor
   end
 
   def doctor_details
-    
+    puts "Name : #{@name}"
 
-  private :doctor_id
-  protected :salary
+    unless @specialization.empty?
+      puts "specialization : #{@specialization}"
+    else
+      puts "specialization not assigned"
+    end
+
+    puts "Experience : #{@experience}"
+
+    if @experience > 10
+      puts "Senior Doctor"
+    elsif @experience > 5
+      puts "Mid level Doctor"
+    else
+      puts "junior Doctor"
+    end
+  end
+
+  def compare_salary(other_doctor)
+    # calling protected method on other object
+    if other_doctor.salary > salary
+      puts "#{other_doctor.name} has higher salary"
+    else
+      puts "#{@name} has higher salary"
+    end
+  end
+
+  def update_salary(new_salary)
+    if new_salary < 0
+      puts "Invalid salary"
+    else
+      @salary = new_salary
+      puts "Salary updated successfully"
+    end
+  end
+
+  private
+  def doctor_id
+    @doctor_id
+  end
+
+  protected
+  def salary
+    @salary
+  end
+end
+
+doc1 = Doctor.new("Dr. Amit", "Cardiology", 80000, 101, 12)
+
+doc2 = Doctor.new("Dr. Riya", "Neurology", 95000, 102, 8)
+
+doc1.welcome_message
+
+doc1.doctor_details
+# doc2.doctor_details
+
+doc1.compare_salary(doc2)
+
+doc1.update_salary(98000)
